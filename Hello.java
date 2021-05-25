53af72baebb0006b413a93590512ec2b24b35431
Tom4-dev <84810006+Tom4-dev@users.noreply.github.com>
2021-05-25 15:54:25 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+public static void main(String[] args){
+
+System.out.println("Chris Straus");
+System.out.println("Hello Java");
+System.out.println("Testing if Java works on our computer system, an odyssey");
+System.out.println("05-25-2021");
+System.out.println("CMSC 255 - C01");
+System.out.println();
+}
+	
+}
